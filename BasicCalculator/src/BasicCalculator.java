3ed72129208790381a641933b8585c88ffe35554
karyRmartinez <karymartinez@pursuit.org>
2023-10-11 12:29:16 -0400

    progress on step 1 in exercise

---
+++ BasicCalculator/src/BasicCalculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        system.out.print("");
+        double numberOne = scanner.nextDouble();
+
+        System.out.print("");
+        double numberTwo = scanner.nextDouble();
+
+        System.out.println("possible calcilations:");
+        System.out.println("(A)add");
+        System.out.println("(S)subtract");
+        System.out.println("(M)multiply");
+        System.out.println("slect option: ");
+
+        char M = scanner.next().charAt(0);
+        float result = 0;
+
+    //if (M == "A ")  {
+     //   result = numberOne + numberTwo;
+      //  System.out.println(numberOne + "+" + numberTwo"=" + result);
+    }
+    }
+
+}
